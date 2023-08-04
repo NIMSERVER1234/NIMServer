8f63287f1bb498f607a08a7e86ea75a15ea1104a
NIMSERVER1234 <141429283+NIMSERVER1234@users.noreply.github.com>
2023-08-04 18:33:16 +0330

    Update Edit.php

--- Edit.php
+++ Edit.php
@@ -1,8 +1,8 @@
 <?php
-if(isset($_POST['text']) && isset($_POST['filename'])) {
+if(isset($_POST['text']) && isset($_POST['namefile1234'])) {
     $text = $_POST['text'];
 
-    $filename = realpath('./') . '/' . basename($_POST['filename']) . '.json';
+    $filename = realpath('./') . '/' . basename($_POST['namefile1234']) . '.json';
 
         if(file_put_contents($filename, $text) !== false) {
             echo "Edited successfully.";
